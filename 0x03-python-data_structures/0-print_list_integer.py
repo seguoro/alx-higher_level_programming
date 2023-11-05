4d0012095e4b84053c26911c5c92b209ee3bc09f
DualB Seguoro <aymanelamsouguer@gmail.com>
2023-11-05 11:52:54 +0100

    $1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,6 @@
 
 
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{:d}".format(num))
+    if my_list:
+        for num in my_list:
+            print("{:d}".format(num))
