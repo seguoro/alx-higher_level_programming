f5753cf496f58f05a6d1d256473ec2ebf63a3280
DualB Seguoro <aymanelamsouguer@gmail.com>
2023-11-05 11:46:08 +0100

    $1

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -2,5 +2,5 @@
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').\
     print_reversed_list_integer
 
-my_list = []
+my_list = [1, 2, 3, 4, 4]
 print_reversed_list_integer(my_list)
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,6 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list == []:
-        return None
-    for num in reversed(my_list):
-        print("{:d}".format(num))
+    if my_list:
+        for elm in my_list[::-1]:
+            print("{:d}".format(elm))
