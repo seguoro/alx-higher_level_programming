1af0cf2a6c6f3d625978c1949ed7c10b57445610
DualB Seguoro <aymanelamsouguer@gmail.com>
2023-11-07 20:07:38 +0100

    $1

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if idx >= len(my_list) - 1 or idx < 0:
-        return my_list
-    else:
-        my_list.remove(my_list[idx])
-        return my_list
+    if my_list:
+        if idx >= len(my_list) - 1 or idx < 0:
+            return my_list
+        else:
+            my_list.remove(my_list[idx])
+            return my_list
