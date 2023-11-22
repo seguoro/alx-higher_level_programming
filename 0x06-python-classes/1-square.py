0bcfada36cc2e3a66b267187222ca7c5203c32aa
DualB Seguoro <aymanelamsouguer@gmail.com>
2023-11-23 00:01:33 +0100

    $1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, side_length):
+        """Constructor.
+        Args:
+            side_length: length of side of the square.
+        """
+        self.__side_length = side_length
